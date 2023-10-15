6c92d2d68c0c86f74ec0c0e89ea52f1d487bb922
Annie Ross <annieross10@outlook.com>
2023-10-15 16:53:37 +0100

    install libraries

---
+++ brushstrokes_by_annie/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for brushstrokes_by_annie project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brushstrokes_by_annie.settings')
+
+application = get_wsgi_application()
